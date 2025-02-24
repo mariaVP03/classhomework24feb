1698891531d1941040228c0e0e88bc47946cbb4e
mariaVP03 <mariavazquezpinedo@student.ie.edu>
2025-02-24 14:13:11 +0100

    app_24feb.py

--- app_24feb.py
+++ app_24feb.py
@@ -51,5 +51,5 @@ def main():
         result = prediction(classifier, Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
         st.success(f'Your loan is {result}')
 
-if _name_ == '_main_': 
+if __name__ == '_main_': 
     main()
